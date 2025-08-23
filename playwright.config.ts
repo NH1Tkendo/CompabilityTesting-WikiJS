103beca5e4cba09bd119763c5764855d3deee10e
Ngô Bá Tài <2212453@dlu.edu.vn>
2025-08-23 19:59:04 +0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,1 +1,18 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests', // thư mục chứa spec
+  timeout: 30 * 1000,
+  retries: 1,
+  use: {
+    baseURL: 'http://localhost:3000', // Wiki.js server
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'webkit', // Safari engine
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
 
