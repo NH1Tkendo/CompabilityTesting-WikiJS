82ad6358ade13247ea9274443d32cb998cb8fb0d
Ngô Bá Tài <2212453@dlu.edu.vn>
2025-08-23 22:05:00 +0700

    Update wiki.spec.ts

--- tests/wiki.spec.ts
+++ tests/wiki.spec.ts
@@ -1,3 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('Wiki.js homepage loads on Safari (WebKit)', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Wiki.js/i);
+});
+
 test('Can navigate to login page', async ({ page }) => {
   await page.goto('/');
 
