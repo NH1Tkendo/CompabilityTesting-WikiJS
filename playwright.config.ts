708c73b9996735c96c1e0836c8dbeff7023cb47c
Ngô Bá Tài <2212453@dlu.edu.vn>
2025-08-23 17:25:04 +0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,1 +1,20 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chrome',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    }
+  ],
+});
 
