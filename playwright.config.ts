7898851898d2fb935acf771b8bb0a8bb395ec9dd
Ngô Bá Tài <2212453@dlu.edu.vn>
2025-08-24 10:52:06 +0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,19 +2,36 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 30 * 1000,
-  use: {
-    trace: 'on-first-retry',
+  timeout: 60 * 1000,
+  expect: {
+    timeout: 5000,
   },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 2 : undefined,
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  use: {
+    baseURL: 'http://localhost:3000',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+
   projects: [
     {
-      name: 'chrome',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    }
+      name: 'msedge',
+      use: {
+        channel: 'msedge',   // Edge browser
+        viewport: { width: 1280, height: 720 },
+        headless: true,
+      },
+    },
   ],
 });
-
